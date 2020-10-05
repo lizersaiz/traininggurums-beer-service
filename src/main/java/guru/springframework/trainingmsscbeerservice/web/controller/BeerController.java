7e7a54a4e66f788009d2b58061e6a74c4f0d893a
lizersaiz <lizer.saiz.ads@gmail.com>
2020-10-05 11:49:42 +0200

    fix controller

--- src/main/java/guru/springframework/trainingmsscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/trainingmsscbeerservice/web/controller/BeerController.java
@@ -9,9 +9,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import guru.springframework.trainingmsscbeerservice.web.model.BeerDto;
 
+@RequestMapping("/api/v1/beer")
+@RestController
 public class BeerController {
 
 	@GetMapping("/{beerId}")
