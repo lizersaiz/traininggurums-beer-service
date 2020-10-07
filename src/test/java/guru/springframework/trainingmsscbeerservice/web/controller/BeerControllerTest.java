7cb2ab481c41dea9dee0d46a8ea210e0e68e86d2
lizersaiz <lizer.saiz.ads@gmail.com>
2020-10-07 19:47:44 +0200

    extend beer controller test. Closes #13

--- src/test/java/guru/springframework/trainingmsscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/trainingmsscbeerservice/web/controller/BeerControllerTest.java
@@ -1,21 +1,28 @@
 package guru.springframework.trainingmsscbeerservice.web.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import guru.springframework.trainingmsscbeerservice.domain.Beer;
+import guru.springframework.trainingmsscbeerservice.repository.BeerRepository;
 import guru.springframework.trainingmsscbeerservice.web.model.BeerDto;
 import guru.springframework.trainingmsscbeerservice.web.model.BeerStyleEnum;
 
@@ -28,8 +35,13 @@ class BeerControllerTest {
 	@Autowired
 	ObjectMapper objectMapper;
 	
+	@MockBean
+	BeerRepository beerRepository;
+	
 	@Test
 	void getBeerById() throws Exception {
+		
+		given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
 		
 		mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk());
