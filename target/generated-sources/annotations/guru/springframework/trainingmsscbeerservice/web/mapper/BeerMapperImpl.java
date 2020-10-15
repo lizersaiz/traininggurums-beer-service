8465be947b01abcfe29dd15946a0b200a51b5202
lizersaiz <lizer.saiz.ads@gmail.com>
2020-10-15 02:36:32 +0200

    change data init to sql file. closes #1

--- target/generated-sources/annotations/guru/springframework/trainingmsscbeerservice/web/mapper/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/trainingmsscbeerservice/web/mapper/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-10-13T20:03:16+0200",
+    date = "2020-10-15T02:31:14+0200",
     comments = "version: 1.3.0.Final, compiler: Eclipse JDT (IDE) 1.3.1100.v20200828-0941, environment: Java 14.0.2 (Oracle Corporation)"
 )
 @Component
