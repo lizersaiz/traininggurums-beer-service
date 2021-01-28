da59476390a80fde374b7e755295d5448d9e725c
lizersaiz <lizer.saiz.ads@gmail.com>
2021-01-28 19:48:32 +0100

    update pom with bom

--- target/generated-sources/annotations/guru/springframework/trainingmsscbeerservice/web/mapper/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/trainingmsscbeerservice/web/mapper/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-10-23T12:40:27+0200",
+    date = "2021-01-28T19:41:46+0100",
     comments = "version: 1.3.0.Final, compiler: Eclipse JDT (IDE) 1.3.1100.v20200828-0941, environment: Java 14.0.2 (Oracle Corporation)"
 )
 @Component
