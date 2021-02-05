9e6173856eb912d5afb762fdfc5c9326ffbedcd2
lizersaiz <lizer.saiz.ads@gmail.com>
2021-02-05 10:30:11 +0100

    brewing service

--- src/main/java/guru/springframework/trainingmsscbeerservice/repository/BeerRepository.java
+++ src/main/java/guru/springframework/trainingmsscbeerservice/repository/BeerRepository.java
@@ -4,13 +4,14 @@ import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import guru.springframework.trainingmsscbeerservice.domain.Beer;
 import guru.springframework.trainingmsscbeerservice.web.model.BeerStyleEnum;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
 
 	Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest);
 
